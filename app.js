c2775fb976b53a0b35f259e540f911de4680a7bc
JHON MAYCON SILVA PREVITERA <125268@pmenos.com.br>
2023-02-22 13:32:36 -0300

    mudando css section2 e app.js

--- app.js
+++ app.js
@@ -1,32 +1,74 @@
+const botaoMonteSeuPedido = document.querySelector('.btn-monte-seu-pedido')
+const botaoEscolhaPaleta = document.querySelector('.btn-monte-seu-paleta')
+const arraySabores = document.getElementsByName('slide')
+const recipientes = document.getElementsByClassName('sorvete_recipientes')
+const selecionandoSaborMais = document.querySelector('.button-quantidade-sorvete-mais')
+const selecionandoSaborMenos = document.querySelector('.button-quantidade-sorvete-menos')
+const selecionandoRecipientes = document.getElementsByClassName('sorvete_recipientes')
+const desapareceTela = document.querySelector('.page1')
+const apareceTela2 = document.querySelector('.sorvete')
+const apareceTela3 = document.querySelector('.paletas')
+var sabor = [];
 
-//cria obejto sorvete
-const sorvete = []
-const trasformandoObjeto = () => {    
-   for(i=0;i<arraySabores.length;i++) {
-    sorvete.push({sabor:arraySabores[i].value})    
-   } 
+
+//criando classe de objeto sorvete
+class montagem {
+    constructor(sabor) {
+        this.sabor = sabor
+    }
 }
-trasformandoObjeto()
 
-const montaSorvete = (sorvete) =>{
-    
+//Recebe o valor do sabor
+const trasformandoObjeto = () => {
+    for (i = 0; i <= arraySabores.length; i++) {
+        if (arraySabores[i].checked) {
+            valor = arraySabores[i].value
+            sabor.push(valor)
+            return
+        }
+    }
+    return sabor
+}
+var sacola = {
+}
+//Montando sorvete
+function adicionandoValor() {
+    if (sabor.length <= 3) {
+        trasformandoObjeto()
+        sorvete = new montagem(sabor)
+
+    }
+    var sacola = {
+        sabor: sorvete,
+        quantidade: "",
+        recipientes: "",
+        corbetura: "",
+    };
+    console.log(sacola);
+    return sorvete
 }
 
+//quantidade removida
+function removendoValor() {
+    if (sabor.length > 0 && sabor.length <= 4) {
+        sabor.pop()
+    }
+    console.log(sorvete)
+}
 
-
+selecionandoSaborMais.addEventListener('click', adicionandoValor)
+selecionandoSaborMenos.addEventListener('click', removendoValor)
 botaoMonteSeuPedido.addEventListener('click', telaSorvete)
 botaoEscolhaPaleta.addEventListener('click', telaPaleta)
 
 function telaSorvete() {
-    const desapareceTela = document.querySelector('.page1')
-    const apareceTela2 = document.querySelector('.sorvete')
     desapareceTela.style.display = 'none'
     apareceTela2.style.display = 'block'
 }
 
 function telaPaleta() {
-    const desapareceTela = document.querySelector('.page1')
-    const apareceTela3 = document.querySelector('.paletas')
     desapareceTela.style.display = 'none'
     apareceTela3.style.display = 'block'
 }
+
+
